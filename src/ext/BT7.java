de6fd318bec1d23eea3c14262ccb4b113f4158b4
Dmytro Zaichenko <dim2002@ukr.net>
2015-11-04 21:25:27 +0200

    3

--- src/ext/BT7.java
+++ src/ext/BT7.java
@@ -5,6 +5,6 @@ public class BT7 extends Tank {
 
 
     public BT7(){
-
+        super();
     }
 }
--- src/ext/Colors.java
+++ src/ext/Colors.java
@@ -1,8 +1,5 @@
 package ext;
 
-/**
- * Created by admin on 04.11.2015.
- */
 public enum Colors {
     RED, BLACK, BLUE, GREEN;
 }
